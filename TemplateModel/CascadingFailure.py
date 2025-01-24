66a2887a8562cb4537d5065d6dd75074c1522c9d
AymanStitou <aymanstitou900@gmail.com>
2025-01-24 10:10:43 +0100

    Chnaged class to handle also directed networks. Unable to resolve, but we're getting reasonable results. close #3

--- TemplateModel/CascadingFailure.py
+++ TemplateModel/CascadingFailure.py
@@ -3,9 +3,13 @@ import matplotlib.pyplot as plt
 import random
 
 class CascadingFailureSimulation:
-    def __init__(self, num_nodes=25, edges_per_node=2):
+    def __init__(self,G=None):
         # self.G = nx.barabasi_albert_graph(num_nodes, edges_per_node)
-        self.G = nx.erdos_renyi_graph(25,0.2)
+        assert G != None
+        # self.G = nx.erdos_renyi_graph(25,0.2)
+        self.G=G
+        self.CF = 0
+        self.N = len(self.G.nodes)
         for node in self.G.nodes:
             self.G.nodes[node]['load'] = 0
             self.G.nodes[node]['capacity'] = 0
@@ -14,23 +18,24 @@ class CascadingFailureSimulation:
             self.G.nodes[node]['closeness_centrality'] = 0
 
     def calculate_centrality_measures(self):
-        degree_centrality = nx.degree_centrality(self.G)
-        betweenness_centrality = nx.betweenness_centrality(self.G, normalized=True)
+        print(self.N)
+        degree_centrality = nx.degree_centrality(self.G) 
+        betweenness_centrality = nx.betweenness_centrality(self.G, normalized=False)
         closeness_centrality = nx.closeness_centrality(self.G)
 
         for node in self.G.nodes:
-            self.G.nodes[node]['degree_centrality'] = degree_centrality[node]
+            self.G.nodes[node]['degree_centrality'] = degree_centrality[node] * (self.N -1)
             self.G.nodes[node]['betweenness_centrality'] = betweenness_centrality[node]
             self.G.nodes[node]['closeness_centrality'] = closeness_centrality[node]
 
     def calculate_initial_load(self, centrality_type='degree'):
         for node in self.G.nodes:
             if centrality_type == 'degree':
-                self.G.nodes[node]['load'] = self.G.nodes[node]['degree_centrality'] * 10
+                self.G.nodes[node]['load'] = self.G.nodes[node]['degree_centrality']
             elif centrality_type == 'betweenness':
-                self.G.nodes[node]['load'] = self.G.nodes[node]['betweenness_centrality'] * 10
+                self.G.nodes[node]['load'] = self.G.nodes[node]['betweenness_centrality']
             elif centrality_type == 'closeness':
-                self.G.nodes[node]['load'] = self.G.nodes[node]['closeness_centrality'] * 10
+                self.G.nodes[node]['load'] = self.G.nodes[node]['closeness_centrality']
 
     def calculate_capacity(self, alpha=0.2, beta=1.5):
         for node in self.G.nodes:
@@ -40,25 +45,45 @@ class CascadingFailureSimulation:
     def simulate_cascading_failure(self, initial_failures):
         failed_nodes = set(initial_failures)
         queue = list(initial_failures)
-
+        NA = len(initial_failures)
         # I think something goes wrong here
         while queue:
             node = queue.pop(0)
-            neighbors = list(self.G.neighbors(node))
+            # neighbors = list(self.G.neighbors(node))
+            if self.G.is_directed():
+                neighbors = list(self.G.successors(node))
+            else:
+                neighbors = list(self.G.neighbors(node))
             # print(list(self.G.neighbors(node)))
-            sum_neighbours = sum([self.G.nodes[neighbor]['load'] for neighbor in neighbors])
+            sum_neighbours = sum([self.G.nodes[neighbor]['capacity'] for neighbor in neighbors])
             # print(sum_neighbours)
             if neighbors:
                 # load_share = self.G.nodes[node]['load'] / len(neighbors)
+                # print(node, neighbors, self.G.nodes[0]['capacity'])
+                # assert sum_neighbours != 0, f"Neighbours of node {node}: {neighbors}"
+
                 for neighbor in neighbors:
                     if neighbor not in failed_nodes:
-                        self.G.nodes[neighbor]['load'] += self.G.nodes[node]['load'] * self.G.nodes[neighbor]['load']/sum_neighbours
-                        print(self.G.nodes[neighbor]['load'],self.G.nodes[neighbor]['capacity'])
-                        if self.G.nodes[neighbor]['load'] > self.G.nodes[neighbor]['capacity']:
-                            failed_nodes.add(neighbor)
-                            queue.append(neighbor)
 
-        return failed_nodes
+                        if sum_neighbours == 0:
+                             # if there are neighbours but they have no initial load
+                             self.G.nodes[neighbor]['load'] += self.G.nodes[node]['load'] / len(neighbors)
+                             failed_nodes.add(neighbor)
+                             queue.append(neighbor)
+                        else:
+                            # self.G.nodes[neighbor]['load'] += self.G.nodes[node]['load'] * self.G.nodes[neighbor]['load']/sum_neighbours
+                            redistributed_load = self.G.nodes[node]['load'] * (self.G.nodes[neighbor]['capacity'] / sum_neighbours)
+                            self.G.nodes[neighbor]['load'] += redistributed_load
+                            print(node,neighbor,self.G.nodes[neighbor]['load'],self.G.nodes[neighbor]['capacity'])
+                            if self.G.nodes[neighbor]['load'] > self.G.nodes[neighbor]['capacity']:
+                                failed_nodes.add(neighbor)
+                                queue.append(neighbor)
+
+        # N = len(self.G.nodes)
+
+        self.CF = NA / (len(failed_nodes) * self.N)
+
+        return failed_nodes, self.CF
 
     def visualize_network(self, failed_nodes):
         pos = nx.spring_layout(self.G)
--- TemplateModel/main.py
+++ TemplateModel/main.py
@@ -1,25 +1,28 @@
 from CascadingFailure import CascadingFailureSimulation
+import networkx as nx
+
+G = nx.read_graphml("replicated network/custom_network.graphml")
+mapping = {node: int(node) for node in G.nodes()}
+G = nx.relabel_nodes(G, mapping)
 
 
-simulation = CascadingFailureSimulation(num_nodes=100, edges_per_node=3)
+simulation = CascadingFailureSimulation(G)
 
 
 simulation.calculate_centrality_measures()
+simulation.print_centrality_measures()
+
+simulation.calculate_initial_load(centrality_type="degree")
 
 
-simulation.calculate_initial_load()
+simulation.calculate_capacity(alpha=0.2, beta=1.5)
 
 
-simulation.calculate_capacity(alpha=0.7, beta=1)
-
-
-initial_failures = [0]  
-failed_nodes = simulation.simulate_cascading_failure(initial_failures)
-
+initial_failures = [11]  
+failed_nodes, CF = simulation.simulate_cascading_failure(initial_failures)
+print("CF:", CF)
 
 simulation.visualize_network(failed_nodes)
 
 
 print("Failed nodes:", failed_nodes)
-
-simulation.print_centrality_measures()
