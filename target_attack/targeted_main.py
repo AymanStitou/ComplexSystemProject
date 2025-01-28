2987bb575caba48c2d0428749728caa37fcd68c6
Axegky <132951747+Axegky@users.noreply.github.com>
2025-01-28 23:41:31 +0100

    add results for the target attack with varying beta

--- target_attack/targeted_main.py
+++ target_attack/targeted_main.py
@@ -5,22 +5,30 @@ import random
 import pandas as pd
 import numpy as np
 
-def run_simulation(G, alpha, initial_failures, centrality_type, simulation, beta=1):
+def run_simulation(initial_failures, centrality_type, simulation, alpha=0.2, beta=1, alpha_list=None, beta_list=None):
     """
     Run the cascading failure simulation for a specific network and centrality measure.
     It returns a list of the number of failed nodes for each alpha value.
     """
     I_list = []
 
-    for a in alpha:
-        simulation.calculate_initial_load(centrality_type=centrality_type)
-        simulation.calculate_capacity(alpha=a, beta=beta)  # Fix beta to 1
-        _, _, I, _ = simulation.simulate_cascading_failure(initial_failures)
-        I_list.append(I)
+    if alpha_list is not None: 
+        for a in alpha_list:
+            simulation.calculate_initial_load(centrality_type=centrality_type)
+            simulation.calculate_capacity(alpha=a, beta=beta)  
+            _, _, I, _ = simulation.simulate_cascading_failure(initial_failures)
+            I_list.append(I)
+    
+    if beta_list is not None: 
+        for b in beta_list: 
+            simulation.calculate_initial_load(centrality_type=centrality_type)
+            simulation.calculate_capacity(alpha=alpha, beta=b) 
+            _, _, I, _ = simulation.simulate_cascading_failure(initial_failures)
+            I_list.append(I)
 
     return I_list
 
-def simulate_and_average(G, alpha, centrality_types, num_simulations=25, beta=1, target_attack=False):
+def simulate_and_average(G, centrality_types, num_simulations=25, target_attack=False, alpha=0.2, beta=1, alpha_list=None, beta_list=None):
     """
     Simulate the cascading failure multiple times and calculate the mean fraction of failed nodes for each centrality type.
     Return a dictionary with centrality measures as keys and mean I_list as values.
@@ -34,8 +42,12 @@ def simulate_and_average(G, alpha, centrality_types, num_simulations=25, beta=1,
     if target_attack: 
         for centrality in centrality_types:
             initial_failures = simulation.rank_centrality(centrality, num_failures)
-            I = run_simulation(G, alpha, initial_failures, centrality, simulation, beta=beta)
-            print(I)
+            if alpha_list is not None: 
+                I = run_simulation(initial_failures, centrality, simulation, alpha_list=alpha_list, beta=beta)
+            elif beta_list is not None: 
+                I = run_simulation(initial_failures, centrality, simulation, beta_list=beta_list, alpha=alpha)
+            else: 
+                raise ValueError
             results[centrality] = I
 
         return results
@@ -44,7 +56,12 @@ def simulate_and_average(G, alpha, centrality_types, num_simulations=25, beta=1,
         for _ in range(num_simulations, attacked_type='random'):
             initial_failures = random.sample(range(1,total_nodes-1), num_failures)
             for centrality in centrality_types:
-                I = run_simulation(G, alpha, initial_failures, centrality, simulation, beta=beta)
+                if alpha_list is not None: 
+                    I = run_simulation(initial_failures, centrality, simulation, alpha_list=alpha_list, beta=beta)
+                elif beta_list is not None: 
+                    I = run_simulation(initial_failures, centrality, simulation, beta_list=beta_list, alpha=alpha)
+                else: 
+                    raise ValueError
                 results[centrality] = I
 
         # Compute mean I_list for each centrality type across simulations
@@ -52,7 +69,32 @@ def simulate_and_average(G, alpha, centrality_types, num_simulations=25, beta=1,
         
         return mean_results
 
+def plot_line_graph(results, alpha=0.2, beta=1, alpha_list=None, beta_list=None, network_type=None, file_name=None): 
+    # plot the figures for the three network
+    plt.figure(figsize=(10, 6))
+    for centrality, mean_result in results.items():
+        if alpha_list is not None:
+            plt.plot(alpha_list, mean_result, label=f"{centrality.capitalize()} Centrality", marker='o')
+            plt.title(fr"Mean Fraction of Failed Nodes vs. $\alpha$ ({network_type}), with $\beta$={beta}")
+            plt.xlabel(fr"$\alpha$")
+        elif beta_list is not None:
+            plt.plot(beta_list, mean_result, label=f"{centrality.capitalize()} Centrality", marker='o')
+            plt.title(fr"Mean Fraction of Failed Nodes vs. $\beta$ ({network_type}), with $\alpha$={alpha}")
+            plt.xlabel(fr"$\beta$")
+        else:
+            raise ValueError 
+    plt.ylabel("Mean Fraction of Failed Nodes (I)")
+    plt.legend()
+    plt.grid()
+    plt.show()
 
+    if file_name is not None: 
+        if alpha_list is not None: 
+            plt.savefig(fr'target_attack/result_graph/{file_name}_beta_{beta}.png') 
+        elif beta_list is not None: 
+            plt.savefig(fr'target_attack/result_graph/{file_name}_alpha_{alpha}.png') 
+        else: 
+            raise ValueError
 
 def load_results_from_csv(filename):
     """
@@ -68,17 +110,21 @@ def load_results_from_csv(filename):
     return alpha, results
 
 
-def save_results_to_csv(results, alpha, filename):
+def save_results_to_csv(results, filename, alpha_list=None, beta_list=None):
     """
     Save simulation results to a CSV file.
 
     """
     df = pd.DataFrame(results)
-    df.insert(0, "Alpha", alpha)
+    if alpha_list is not None: 
+        df.insert(0, "Alpha", alpha_list)
+    elif beta_list is not None: 
+        df.insert(0, "Beta", beta_list)
+    else: 
+        raise ValueError
     
     df.to_csv(filename, index=False)
     print(f"Results saved to {filename}")
-
 
 # load the toy network
 G = nx.read_graphml("degree_distribution/toy_network_undirected.graphml")
@@ -97,56 +143,45 @@ G3 = nx.relabel_nodes(G3, mapping3)
 
 
 # initialize parameters and empty lists
-alpha = np.linspace(0,1.2,20)
+alpha_list = np.linspace(0,1.2,20)
+beta_list = np.linspace(1,1.5,20)
+alpha_value = 0.3
 beta_value = 1.05
 centrality_types = ["degree", "betweenness", "closeness"]
 
-# start simulation of the toy network
-results_toy = simulate_and_average(G, alpha, centrality_types, beta=beta_value, target_attack=True)
-print(f"The result for the toy network is: {results_toy}")
+# # start simulation of the toy network with varying alpha
+# results_toy_varing_alpha = simulate_and_average(G, centrality_types, target_attack=True, beta=beta_value, alpha_list=alpha_list)
+# print(f"The result for the toy network with varing alpha is: {results_toy_varing_alpha}")
+# plot_line_graph(results_toy_varing_alpha, beta=beta_value, alpha_list=alpha_list, network_type="Toy Network")
+# save_results_to_csv(results_toy_varing_alpha, "toy_network_beta_results.csv", alpha_list)
 
-# start simulation of the iceland network
-results_ice = simulate_and_average(G2, alpha, centrality_types, beta=beta_value, target_attack=True)
-print(f"The result for the iceland network is: {results_ice}")
+# # start simulation of the iceland network with varying alpha
+# results_ice_varing_alpha = simulate_and_average(G2, centrality_types, target_attack=True, beta=beta_value, alpha_list=alpha_list)
+# print(f"The result for the iceland network with varing alpha is: {results_ice_varing_alpha}")
+# plot_line_graph(results_ice_varing_alpha, beta=beta_value, alpha_list=alpha_list, network_type="Iceland Network")
+# save_results_to_csv(results_ice_varing_alpha, "iceland_network_beta_results.csv", alpha_list)
 
-# start simulation of the US network
-results_us = simulate_and_average(G3, alpha, centrality_types, beta=beta_value, target_attack=True)
-print(f"The result for the US network is: {results_us}")
+# # start simulation of the US network with varying alpha
+# results_us_varing_alpha = simulate_and_average(G3, centrality_types, target_attack=True, beta=beta_value, alpha_list=alpha_list)
+# print(f"The result for the US network with varing alpha is: {results_us_varing_alpha}")
+# plot_line_graph(results_us_varing_alpha, beta=beta_value, alpha_list=alpha_list, network_type="US Network")
+# save_results_to_csv(results_us_varing_alpha, "US_network_beta_results.csv", alpha_list)
 
+# start simulation of the toy network with varying beta
+results_toy_varing_beta = simulate_and_average(G, centrality_types, target_attack=True, alpha=alpha_value, beta_list=beta_list)
+print(f"The result for the toy network with varing beta is: {results_toy_varing_beta}")
+plot_line_graph(results_toy_varing_beta, alpha=alpha_value, beta_list=beta_list, network_type="Toy Network")
+save_results_to_csv(results_toy_varing_beta, "toy_network_alpha_results.csv", beta_list=beta_list)
 
-# plot the figures for the three network
-plt.figure(figsize=(10, 6))
-for centrality, mean_result in results_toy.items():
-    plt.plot(alpha, mean_result, label=f"{centrality.capitalize()} Centrality", marker='o')
-plt.title("Mean Fraction of Failed Nodes vs. Alpha (Toy Network)")
-plt.xlabel("Alpha")
-plt.ylabel("Mean Fraction of Failed Nodes (I)")
-plt.legend()
-plt.grid()
-plt.show()
+# start simulation of the iceland network with varying beta
+results_ice_varing_beta = simulate_and_average(G2, centrality_types, target_attack=True, alpha=alpha_value, beta_list=beta_list)
+print(f"The result for the iceland network with varing beta is: {results_ice_varing_beta}")
+plot_line_graph(results_ice_varing_beta, alpha=alpha_value, beta_list=beta_list, network_type="Iceland Network")
+save_results_to_csv(results_ice_varing_beta, "iceland_network_alpha_results.csv", beta_list=beta_list)
 
-plt.figure(figsize=(10, 6))
-for centrality, mean_result in results_ice.items():
-    plt.plot(alpha, mean_result, label=f"{centrality.capitalize()} Centrality", marker='o')
-plt.title("Mean Fraction of Failed Nodes vs. Alpha (Iceland Network)")
-plt.xlabel("Alpha")
-plt.ylabel("Mean Fraction of Failed Nodes (I)")
-plt.legend()
-plt.grid()
-plt.show()
-
-plt.figure(figsize=(10, 6))
-for centrality, mean_result in results_us.items():
-    plt.plot(alpha, mean_result, label=f"{centrality.capitalize()} Centrality", marker='o')
-plt.title("Mean Fraction of Failed Nodes vs. Alpha (US Network)")
-plt.xlabel("Alpha")
-plt.ylabel("Mean Fraction of Failed Nodes (I)")
-plt.legend()
-plt.grid()
-plt.show()
-
-# save simulation results to csv file:
-save_results_to_csv(results_toy, alpha, "toy_network_results.csv")
-save_results_to_csv(results_ice, alpha, "iceland_network_results.csv")
-save_results_to_csv(results_us, alpha, "US_network_results.csv")
+# start simulation of the US network with varying beta
+results_us_varing_beta = simulate_and_average(G3, centrality_types, target_attack=True, alpha=alpha_value, beta_list=beta_list)
+print(f"The result for the US network with varing beta is: {results_us_varing_beta}")
+plot_line_graph(results_us_varing_beta, alpha=alpha_value, beta_list=beta_list, network_type="US Network")
+save_results_to_csv(results_us_varing_beta, "US_network_alpha_results.csv", beta_list=beta_list)
 
